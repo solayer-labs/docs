be70427db78706e5f9ae147168bff59ff5466d9f
Jason <lxjpub@gmail.com>
2024-02-12 18:08:14 -0800

    Update social card image path

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -55,7 +55,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/social-card.jpg',
     navbar: {
       title: 'Solayer | Docs',
       logo: {
